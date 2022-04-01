802933af1d1b3494f708074d8b91f3291df4ceb5
Koval-Nataliya <79361889+Koval-Nataliya@users.noreply.github.com>
2022-04-01 10:34:25 +0300

    Update allocator.cpp

--- 01/allocator.cpp
+++ 01/allocator.cpp
@@ -1,18 +1,43 @@
-#pragma once
+#include <iostream>
+#include "allocator.hpp"
 
-#include <gtest/gtest.h>
+void Allocator::makeAllocator(size_t maxSize) {
+    if (!maxSize) {
+    	return;
+    }
+    if (allocated) {
+        delete[] memory;
+        allocated = false;
+    }
+    allocated = true;
+    memory = new char[maxSize];
+    offset = 0;
+    allocated_size = maxSize;
+}
 
-class Allocator 
-{
-    bool allocated = false;
-    size_t allocated_size;
-    size_t offset;
-    char *memory;
-public:
-    bool IsAllocated();
-    size_t Get_of();
-    void makeAllocator(size_t maxSize);
-    char* alloc(size_t size);
-    void reset();
-    ~Allocator();
-};
+char* Allocator::alloc(size_t size) {
+    if (!size) {
+    	return nullptr;
+    }	
+    size_t new_offset = offset + size;
+    if (new_offset > allocated_size)
+    {
+        return nullptr;
+    }
+    char *block = &memory[offset];
+    offset = new_offset;
+    return block;
+}
+size_t Allocator::Get_of() {
+    return offset;
+}
+bool Allocator::IsAllocated() {
+    return allocated;
+}
+void Allocator::reset() {
+    offset = 0;
+}
+
+Allocator::~Allocator() {
+    delete[] memory;
+}
